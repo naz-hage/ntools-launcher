48b7fa8a4d8ae4e92d131d6b3dee8b5221df1b85
nhage <nhage@comcast.net>
2024-01-28 10:53:33 -0600

    Fix Actions build failure

--- LauncherTests/ShellUtilityTests.cs
+++ LauncherTests/ShellUtilityTests.cs
@@ -17,7 +17,13 @@ namespace Ntools.Tests
             // Arrange
             var file = "git.exe";
             var expected = @"C:\Program Files\git\cmd\git.exe";
-
+            // on GitHub Actions, git.exe is 
+            var githubActions = Environment.GetEnvironmentVariable("GITHUB_ACTIONS", EnvironmentVariableTarget.User);
+            if (githubActions != null && githubActions.Equals("true", StringComparison.OrdinalIgnoreCase))
+            {
+                expected = @"C:\Program Files\Git\bin\git.exe";
+            }
+            
             // Act
             var actual = ShellUtility.GetFullPathOfFile(file);
 
